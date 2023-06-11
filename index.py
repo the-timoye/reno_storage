40458e2d5e02a894d19c5e87bb2c0e4e83e55e5e
the-timoye <thetimoye@gmail.com>
2023-06-11 17:55:47 +0100

    feat: call all extract and transform functions

--- index.py
+++ index.py
@@ -1,32 +1,8 @@
-import configparser
-import pandas as pd
-
-# read .env file
-config = configparser.ConfigParser()
-config.read('.env')
-
-# store variable names
-bucket_url = config['SOURCE']['S3_URL']
-addresses_file = config['FILES']['ADDRESS']
-customers_file = config['FILES']['CUSTOMERS']
-storage_spaces = config['FILES']['STORAGE_SPACES']
-tnxs_I = config['FILES']['TXNS_1']
-tnxs_II = config['FILES']['TXNS_2']
+from etl.extraction import extract_files
+from etl.transform import transform_files
 
 
-# read files from S3
-addresses = pd.read_csv(f"{bucket_url}/{addresses_file}")
-customers = pd.read_json(f"{bucket_url}/{customers_file}")
-storages = pd.read_json(f"{bucket_url}/{storage_spaces}")
-tnxs_I = pd.read_csv(f"{bucket_url}/{tnxs_I}")
-tnxs_II = pd.read_csv(f"{bucket_url}/{tnxs_II}")
 
-
-# save file to S3
-addresses.to_csv('data/addresses.csv')
-customers.to_csv('data/customers.csv')
-storages.to_csv('data/storages.csv')
-tnxs_I.to_csv('data/transactions_I.csv')
-tnxs_II.to_csv('data/transactions_II.csv')
-
-
+if __name__ == "__main__":
+    extract_files()
+    transform_files()
